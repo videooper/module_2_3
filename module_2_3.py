83c5ec0d9b520c38ac62627cd0a41a7d8adffb02
videooper <97392291+videooper@users.noreply.github.com>
2024-11-26 19:32:26 +0300

    Update module_2_3.py
    исправление после проверки

--- module_2_3.py
+++ module_2_3.py
@@ -2,12 +2,11 @@ my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 index = 0
 print('Вывод на консоль:')
 while index < len(my_list):
-    if my_list[index] > 0:
-        print(my_list[index])
+    if my_list[index] == 0:
         index += 1
         continue
-    elif my_list[index] == 0:
-        pass
+    elif my_list[index] > 0:
+        print(my_list[index])
+        index += 1
     elif my_list[index] < 0:
         break
-
